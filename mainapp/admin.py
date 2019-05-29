e6f6c7a64cf42118edbe19f6be61f7526614dbc8
Abhinav Singh <cancrackjee2014@gmail.com>
2019-05-30 00:56:21 +0530

    admin change

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User,Group
 from .models import Student
 
 
+
+
 # Define an inline admin descriptor for Student model
 # which acts a bit like a singleton
 
@@ -23,3 +25,7 @@ class UserAdmin(BaseUserAdmin):
 # Register your models here.
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin,)
+admin.site.register(Complain)
+admin.site.register(CouncilandCell)
+admin.site.register(Club)
+admin.site.register(Notification)
