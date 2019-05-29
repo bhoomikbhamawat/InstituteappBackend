7342cc3eb5aa467cc0f034131db9e2dffccbe4ff
Abhinav Singh <cancrackjee2014@gmail.com>
2019-05-30 00:58:02 +0530

    fix

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User,Group
-from .models import Student
+from .models import *
 
 
 
