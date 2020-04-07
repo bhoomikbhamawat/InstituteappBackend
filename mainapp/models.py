fcbc043f0c9605d99b65a68cd2c965307ad61739
Karyamsetty Martin <54419980+AngryCder@users.noreply.github.com>
2020-04-07 17:57:21 +0530

    removing lost and found

--- mainapp/models.py
+++ mainapp/models.py
@@ -95,28 +95,7 @@ class Exam(models.Model):
 	
 	
 	
-class LostObj(models.Model):
-	name = models.CharField(max_length=100,blank=True)
-	department = models.CharField(max_length=100,blank=True)
-	image = models.ImageField(null=True,blank=True)
-	obj = models.CharField(max_length=20,blank=True)
-	contact = models.IntegerField(blank = True)
-	last_loc = models.CharField(max_length=100,blank=True)
-	
-	def __str__(self):
-		return self.obj
 
-class FoundObj(self):
-	name = models.CharField(max_length=100,blank=True)
-	department = models.CharField(max_length=100,blank=True)
-	image = models.ImageField(null=True,blank=True)
-	obj = models.CharField(max_length=20,blank=True)
-	contact = models.IntegerField(blank = True)
-	last_loc = models.CharField(max_length=100,blank=True)
-	
-	def __str__(self):
-		return self.obj
-	
 	
 	
 	
